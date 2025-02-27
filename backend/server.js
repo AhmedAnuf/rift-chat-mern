00e84b5d383ef89720ca5d295b94cb4e8ae0718b
AhmedAnuf <ahmedanuf11@gmail.com>
2025-02-28 00:13:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5173","https://riftchat.netlify.app/"],
+    origin: ["https://riftchat.netlify.app/", "http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   },
