d7e3e2417ddf1f0483e2d64c622dafb052fb7265
AhmedAnuf <ahmedanuf11@gmail.com>
2025-02-27 23:15:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: [],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   },
